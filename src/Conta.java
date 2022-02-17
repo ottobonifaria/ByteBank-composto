0907b26e14c0cca723396e0cb4a7a4ebee16d397
ottobonifaria <ottobonifaria.live.com>
2022-02-17 11:21:29 -0300

    Composição de objetos

---
+++ src/Conta.java
@@ -0,0 +1,26 @@
+public class Conta {
+     double saldo;
+     int agencia;
+     int numero;
+     Cliente titular;
+
+
+     public void deposita(double valor){
+          this.saldo += valor;
+     }
+     public boolean saca(double valor){
+          if(saldo>= valor){
+               saldo -= valor;
+               return true;
+          }
+          return false;
+     }
+     public boolean transfere(double valor, Conta destino){
+          if(saldo>= valor){
+               saldo -= valor;
+               destino.deposita(valor);
+               return true;
+          }
+          return false;
+     }
+}
